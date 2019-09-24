a7ba33e09a98995067147fe2b12d6bbe64d992e6
Harjeet Singh <harjeetss@harjeet.com>
2019-09-24 17:21:54 +0530

    05.The ListView Builder

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:todoey_flutter/widgets/task_tile.dart';
+import 'package:todoey_flutter/model/task.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+  @override
+  _TasksListState createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [];
+
   @override
   Widget build(BuildContext context) {
     return ListView(
